41c2295fcb11db51d52870e3c69fff5da20f6ab8
danielafrimi <danielafrimi8@gmail.com>
2021-07-28 20:05:31 +0300

    shell script for phoenix cluster

---
+++ Adverserial_Variance/run_binary.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --mail-user=daniel.afrimi@mail.huji.ac.il
+#SBATCH --gres=gpu:1,vmem:3g
+#SBATCH --mem=5g
+#SBATCH --time=2-0
+#SBATCH -c2
+#SBATCH --output=/cs/labs/daphna/daniel023/Adverserial_Variance/model_logs/sbatch_%J.out
+
+module load cuda
+module load torch
+
+source /cs/labs/daphna/daniel023/lab_env/bin/activate
+cd /cs/labs/daphna/daniel023/Adverserial_Variance
+
+python3 main.py
+
