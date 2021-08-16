3b08bbfb32297b9ba6ea8e3478ea668a53b8b700
danielafrimi <danielafrimi8@gmail.com>
2021-08-16 11:50:39 +0300

    Refactor python folder

---
+++ Adversarial_Variety/consts.py
@@ -0,0 +1,7 @@
+CLASSES = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+CLASSES_BINARY = ('cat', 'dog')
+LOGGER_FORMAT = '<magenta>{time:YYYY-MM-DD HH:mm:ss}</magenta> | <level>{message}</level>'
+DATETIME_STRING_FORMAT = '%Y-%m-%d_%H-%M-%S'
+
+N_CLASSES = len(CLASSES)
+CIFAR10_IMAGE_SIZE = 32
